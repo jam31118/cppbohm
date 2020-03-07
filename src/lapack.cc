6d756f053f058ccd486759b8c6ebcad7633970c8
Ahn, Seongjin <jam45@naver.com>
2020-03-08 00:27:45 +0900

    add lapack wrapper and processor

---
+++ src/lapack.cc
@@ -0,0 +1,19 @@
+#include "lapack.hh"
+
+int handle_gesv_info(int info) {
+		if (info != 0) {
+				fprintf(stderr, "[ERROR] Unsuccessful exit from 'dgesv_()'\n");
+				if (info < 0) {
+						fprintf(stderr, 
+										"[ERROR] [info == '%d'] the '%d'-th argument "
+										"had an illegal value\n", info, -info);
+    		}
+    		else if (info > 0) {
+						fprintf(stderr, "[ERROR] [info == '%d'] "
+										"singularity happended\n", info);
+				}
+				return EXIT_FAILURE;
+  	}
+  	return EXIT_SUCCESS;
+}
+
