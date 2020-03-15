b48591cb7fa1cdea4df49d1376093d2722ae9c05
Ahn, Seongjin <jam45@naver.com>
2020-03-15 19:50:52 +0900

    clean-up && commentation

--- src/fd.cc
+++ src/fd.cc
@@ -8,19 +8,32 @@ int eval_f_and_derivs(
 		const size_t Nx_tot, const double dx, const double x0,
 		std::complex<double> *wf_derivs, size_t *is0) 
 {
+
+	// Check arguments
+	// 
 	const double xmin = x0, xmax = x0 + ( Nx_tot - 1 ) * dx;
 	if ((xmin > x) || (x >= xmax)) { return EXIT_FAILURE; }
-	if ((wf[0] != 0.) || (wf[Nx_tot-1] != 0.)) { return EXIT_FAILURE; }
-	
-	const size_t Ns = FD_STENCIL_NUM, il = (size_t) abs( (int) ((x - xmin) / dx));
-//	std::cout << "x, il, (x - xmin) / dx = " << x << ", " << il << ", "<< (x - xmin) / dx << std::endl;
-	const size_t _is0 = (il-1) + (il<1)*(1-il) + (il>Nx_tot-3)*(Nx_tot-3-il);
+
+
+	// Determine the index of spatial point for finite difference stencils: `is0`
+	// 
+	const size_t Ns = FD_STENCIL_NUM;
+	const size_t Nsr = Ns / 2; // number of stencils on right side of il
+	const size_t Nsl = Nsr - (Ns % 2); // number of stencils on left side of il
+	const int _il_int = (int) ((x - xmin) / dx);
+	if (_il_int < 0) { return EXIT_FAILURE; }
+	const size_t il = (size_t) abs(_il_int);
+	const size_t _is0 = (il-Nsl) \
+											+ (il<Nsl)*(Nsl-il) \
+											+ (il>Nx_tot-1-Nsr)*(Nx_tot-1-Nsr-il);
 	*is0 = _is0;
+
+	// Construct matrices for finite difference: `A1d`, `b1d`
+	// 
 	double xs_minus_x[Ns];  // finite difference stencils minus x
 	double *const pxsmax = xs_minus_x + Ns;
 	for (double *pxs=xs_minus_x, val=xmin+_is0*dx-x; 
 			pxs < pxsmax; ++pxs, val += dx) { *pxs = val; }
-
 	double A1d[Ns*Ns];
 	for (double *pA=A1d, *pAmax=A1d+Ns; pA<pAmax; ++pA) { *pA = 1.; }
 	for (double *pAprev=A1d, *pA=A1d+Ns, *pAmax=A1d+Ns*Ns; pA<pAmax; ) {
@@ -37,22 +50,20 @@ int eval_f_and_derivs(
 		*breal = _wf.real(), *bimag = _wf.imag();
 	}
 
+	// Solve finite difference equation Ax=b to get derivatives of wf
+	// 
 	int ipiv[Ns], info;
 	dgesv_((int *)&Ns,(int *)&ncol_b,A1d,(int *)&Ns,ipiv,b1d,(int *)&Ns,&info);
 	if (handle_gesv_info(info) != EXIT_SUCCESS) { return EXIT_FAILURE; }
-//	{ std::cerr << "[ERROR] Failed to solve linear system.\n"; }
 	
+	// Store the results to given array `wf_derivs`
+	//
 	std::complex<double> *pwf_derivs = wf_derivs, *pwf_derivs_max = wf_derivs+Ns;
 	for (double *breal=b1d, *bimag=b1d+Ns; pwf_derivs < pwf_derivs_max; 
 			++breal, ++bimag, ++pwf_derivs)
 	{ *pwf_derivs = {*breal, *bimag}; }
 
-//	for (size_t i=0; i<Ns; ++i) { A1d[i] = 1.; }
-//	for (size_t icol=1; icol<Ns; ++icol) {
-//		for (size_t irow=0; irow<Ns; ++irow) {
-//			A1d[icol*Ns+irow] = A1d[(icol-1)*Ns+irow] * xs_minus_x[irow];
-//		}
-//	}
-
+	// Return
 	return EXIT_SUCCESS;
 }
+
--- test/propagator/box-1d/box-1d.cc
+++ test/propagator/box-1d/box-1d.cc
@@ -19,150 +19,118 @@
 
 int main() {
 
-//	int return_code = EXIT_SUCCESS;
-
-//	double *Vx;
-//	Bohm_Propagator_on_Box_1D prop;
-//	std::complex<double> *wf;
-
+	// Get parameters from input parameter file
+	// 
 	ParamFile param;
 	try {	param = ParamFile("in.param"); }
 	catch (std::exception& e) { 
 		std::cerr << "[ERROR] During constructing param: " 
 			<< e.what() << std::endl; 
+		return EXIT_FAILURE;
 	}
 	catch (...) { 
 		std::cerr << "[ERROR] Failed to construct param file with reason unknown"; 
+		return EXIT_FAILURE;
 	}
-
 	const size_t Nx = param.get_int("Nx");
 	const double dx = param.get_double("dx");
+	const double xmin = param.get_double("xmin");
 	const double dt = param.get_double("dt");
-	const double xmin = 0.0;
-	
 	const size_t Nq = param.get_int("Nq");  // number of particles
 
 
+	// Prepare potential array
+	//
 	double *Vx = new double[Nx];
 	set_to_zeros(Vx, Nx);
 
+
+	// Construct propagator
+	//
 	Bohm_Propagator_on_Box_1D prop(Nx, dx, Vx);
 
-	const size_t Nx_tot = 1 + Nx + 1;
-	std::complex<double> *wf = new std::complex<double>[Nx_tot];
-	set_to_randoms(wf+1, Nx);
-	wf[0] = 0.; wf[Nx_tot-1] = 0.;
 
-	if (prop.propagate_to_ground_state(wf+1, dt, 5000, 1e-10) != EXIT_SUCCESS) {
+	// Prepare initial state
+	// 
+	const size_t Nx_tot = 1 + Nx + 1;
+	std::complex<double> *wf_tot = new std::complex<double>[Nx_tot];
+	std::complex<double> *wf = wf_tot + 1;
+	set_to_randoms(wf, Nx);
+	wf_tot[0] = 0.; wf_tot[Nx_tot-1] = 0.;
+	// Proagate into the state with lowest energy possible
+	if (prop.propagate_to_ground_state(wf, dt, 5000, 1e-10) != EXIT_SUCCESS) {
 		std::cerr << "[ERROR] Failed to propagator to ground state\n";
 		return EXIT_FAILURE;
-//		return_code = EXIT_FAILURE;
-//		goto free_and_return;
 	}
 
-	// Test eval wf_derivs for given paritlce positions
+
+	// Set particle coordinates 
+	// 
 	double *qarr = new double[Nq];
-
 //	set_to_randoms(qarr, Nq, xmin, xmin+(Nx+1)*dx);
-	for (size_t i=0; i<Nq; ++i) { qarr[i] = i * (Nx_tot-1)*dx / (Nq-1); }
+	for (size_t i=0; i<Nq; ++i) { qarr[i] = (i+1) * (Nx_tot-1)*dx / (Nq-1+2); }
 
+
+	// Print particle coordinates
+	//
 	std::cout << "[ LOG ] Particle coordinates: ";
 	for (size_t i=0; i<Nq; ++i) { std::cout << qarr[i] << " "; }
 	std::cout << std::endl;
 
 
-	
-
 	std::complex<double> *wf_qarr = new std::complex<double>[Nq];
 	std::complex<double> *dx_wf_qarr = new std::complex<double>[Nq];
-
-
-
-//	std::cout << "qarr: " << qarr << std::endl;
-//	std::cout << "qarr[0]: " << qarr[0] << std::endl;
-//	std::cout << "qarr[1]: " << qarr[1] << std::endl;
-
-
 
 	std::complex<double> wf_derivs[FD_STENCIL_NUM];
 	int stat;
 	size_t is0;
 	for (size_t i=0; i<Nq; ++i) {
-//		std::cout << "qarr[i]: " << qarr[i] << std::endl;
-		stat = eval_f_and_derivs(qarr[i], wf, Nx_tot, dx, xmin, wf_derivs, &is0);
-//		std::cout << "is0=" << is0 << "x=" << qarr[i] << std::endl;
-//		std::cout << "qarr[i] (after): " << qarr[i] << std::endl;
-
+		stat = eval_f_and_derivs(qarr[i], wf_tot, Nx_tot, dx, xmin, wf_derivs, &is0);
 		if (stat != EXIT_SUCCESS) {
-			std::cout << "[ERROR] Failed to evaluate eval_f_and_derivs\n";
+			std::cout << "[ERROR] Failed to evaluate wf value and derivatives "
+				"(i.e. wf, dx_wf, ...)\n";
+			std::cout << "[ERROR] The particle position x=" << qarr[i] << std::endl;
 			return EXIT_FAILURE;
-//			return_code = EXIT_FAILURE;	
-//			goto free_and_return;
 		}
-
 		wf_qarr[i] = wf_derivs[0];
 		dx_wf_qarr[i] = wf_derivs[1];
-
-//		std::cout << "[ LOG ] wf_derivs of particle x=" << qarr[i] << ": ";
-//		for (size_t is=0; is<FD_STENCIL_NUM; ++is) {
-//			std::cout << wf_derivs[is] << " ";
-//		}	std::cout << std::endl;
 	}
 
 
-//	std::cout << "[ LOG ] Particle coordinates: ";
-//	for (size_t i=0; i<Nq; ++i) { std::cout << qarr[i] << " "; }
-
-
-//	std::cout << "qarr: " << qarr << std::endl;
-//	std::cout << "qarr[0]: " << qarr[0] << std::endl;
-//	std::cout << "qarr[1]: " << qarr[1] << std::endl;
-
-
+	// Construct spatial array
+	//
 	double *xarr = new double[Nx_tot];
-	for (double *px=xarr, *pxmax=xarr+Nx_tot, val=xmin; px<pxmax; ++px, val+=dx) { *px = val; }
+	for (double *px=xarr, *pxmax=xarr+Nx_tot, val=xmin; px<pxmax; ++px, val+=dx)
+	{ *px = val; }
 
 
+	// Store data to files
+	// 
 	std::ofstream xarr_file("xarr.bin", std::ios::binary);
 	xarr_file.write((char *) xarr, Nx_tot * sizeof(double));
 	xarr_file.close();
 
-
-//	std::cout << "Nq: " << Nq << std::endl;
-
 	std::ofstream qarr_file("qarr.bin", std::ios::binary);
 	qarr_file.write((char *) qarr, Nq * sizeof(double));
-//	if (!qarr_file.good()) { 
-//		std::cerr << "[ERROR] Failed to write qarr\n"; return EXIT_FAILURE; }
 	qarr_file.close();
-//	if (! qarr_file) { 
-//		std::cerr << "[ERROR] Something strange with qrr_file\n";
-//		return EXIT_FAILURE;
-//	} 
-
-//	std::cout << "qarr: " << qarr << std::endl;
 
 	std::ofstream wf_qarr_file("wf_qarr.bin", std::ios::binary);
 	wf_qarr_file.write((char *) wf_qarr, Nq * sizeof(std::complex<double>));
 	wf_qarr_file.close();	
 
-
 	std::ofstream dx_wf_qarr_file("dx_wf_qarr.bin", std::ios::binary);
 	dx_wf_qarr_file.write((char *) dx_wf_qarr, Nq * sizeof(std::complex<double>));
 	dx_wf_qarr_file.close();
-
-
 	
 	std::ofstream wf_file("wf.bin", std::ios::binary);
-	wf_file.write((char *) wf, Nx_tot * sizeof(std::complex<double>));
+	wf_file.write((char *) wf_tot, Nx_tot * sizeof(std::complex<double>));
 	wf_file.close();
 
 
-
-//free_and_return:
-//	prop.~Bohm_Propagator_on_Box_1D();
+	// Free memory and return
+	//
 	delete [] Vx;	
-	delete [] wf;
+	delete [] wf_tot;
 
 	delete [] xarr;
 	delete [] qarr;
