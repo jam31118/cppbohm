8db6c49929a1ca4025fd368b584ebc70d30eda78
Ahn, Seongjin <jam45@naver.com>
2020-03-16 00:33:55 +0900

    break into smaller parts

--- include/fd.h
+++ include/fd.h
@@ -5,6 +5,14 @@
 
 #define FD_STENCIL_NUM 4
 
+int eval_is0(const double x, const size_t Nx_tot, const double dx, 
+		const double x0, size_t *is0);
+
+int eval_f_and_derivs_with_is0(
+		const double x, const std::complex<double> *wf, 
+		const double dx, const double x0,
+		std::complex<double> *wf_derivs, size_t is0);
+
 int eval_f_and_derivs(
 		const double x, const std::complex<double> *wf, 
 		const size_t Nx_tot, const double dx, const double x0,
