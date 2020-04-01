ecbb13360126007b9a32734a670f497f61555605
Ahn, Seongjin <jam45@naver.com>
2020-04-01 23:18:48 +0900

    fix missing factorial in fd method for interpolating wf and wf-derivs && add debug routines inside the propagator -- should be separated or by using preprocessors

--- src/fd.cc
+++ src/fd.cc
@@ -4,6 +4,17 @@
 #include <iostream>
 
 
+/**
+ * Evaluate an index of the first stencil (`is0`) for finite difference method,
+ * of a equidistanced (i.e. equal-spacing) spatial grid.
+ *
+ * @param[in] position at which the corresponding first-stencil (`is0`)
+ * @Nx_tot[in] total number of spatial grid points
+ * @dx[in] spacing of the spatial grid
+ * @x0[in] minimum value of the grid
+ * @is0[out] the first-stencil (`is0`) that is found
+ */
+
 int eval_is0(const double x, const size_t Nx_tot, const double dx, 
 		const double x0, size_t *is0) {
 
@@ -17,7 +28,7 @@ int eval_is0(const double x, const size_t Nx_tot, const double dx,
 	// 
 	const size_t Ns = FD_STENCIL_NUM;
 	const size_t Nsr = Ns / 2; // number of stencils on right side of il
-	const size_t Nsl = Nsr - (Ns % 2); // number of stencils on left side of il
+	const size_t Nsl = Nsr-((Ns-1) % 2); // number of stencils on left side of il
 	const int _il_int = (int) ((x - xmin) / dx);
 	if (_il_int < 0) { return EXIT_FAILURE; }
 	const size_t il = (size_t) abs(_il_int);
@@ -30,28 +41,38 @@ int eval_is0(const double x, const size_t Nx_tot, const double dx,
 
 
 
+/**
+ * Evaluate the value of wavefunction and its partial derivatives 
+ * at given coordinate by using finite difference method
+ */
+
 int eval_f_and_derivs_with_is0(
-		const double x, const std::complex<double> *wf, 
+		const double x, const std::complex<double> *wf_tot, 
 		const double dx, const double x0,
 		std::complex<double> *wf_derivs, size_t is0) 
 {
 	const size_t Ns = FD_STENCIL_NUM;
+	if (dx < 0) { return EXIT_FAILURE; }
 
-	// Construct matrices for finite difference: `A1d`, `b1d`
+	//// Construct matrices for finite difference: `A1d`, `b1d`
 	// 
-	double xs_minus_x[Ns];  // finite difference stencils minus x
+	// Construct an array of displacements of each stencil: x_arr[is]-x
+	double xs_minus_x[Ns];  // values of finite difference stencils minus x
 	double *const pxsmax = xs_minus_x + Ns;
 	for (double *pxs=xs_minus_x, val=x0+is0*dx-x; 
 			pxs < pxsmax; ++pxs, val += dx) { *pxs = val; }
+
 	double A1d[Ns*Ns];
 	for (double *pA=A1d, *pAmax=A1d+Ns; pA<pAmax; ++pA) { *pA = 1.; }
-	for (double *pAprev=A1d, *pA=A1d+Ns, *pAmax=A1d+Ns*Ns; pA<pAmax; ) {
+	double itay = 1;  // for making n! under the derivative of f in taylor series
+	for (double *pAprev=A1d, *pA=A1d+Ns, *pAmax=A1d+Ns*Ns; pA<pAmax; itay+=1.) {
 		for (double *pxs=xs_minus_x; pxs<pxsmax; ++pxs, ++pAprev, ++pA) 
-		{ *pA = (*pAprev) * (*pxs);	}
+		{ *pA = (*pAprev) * (*pxs) / itay;	}
 	}
 	const size_t ncol_b = 2;
 	double b1d[ncol_b*Ns];
-	const std::complex<double> *pwf = wf+is0, *pwfmax = wf+is0+Ns;
+	const std::complex<double> *pwf = wf_tot+is0;
+	const std::complex<double> *pwfmax = pwf + Ns;
 	std::complex<double> _wf;
 	for (double *breal=b1d, *bimag=b1d+Ns; pwf < pwfmax; ++breal, ++bimag, ++pwf)
 	{ 
--- src/propagator/bohm-propagator-on-box-1d.cc
+++ src/propagator/bohm-propagator-on-box-1d.cc
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <complex>
+#include <fstream>
 
 
 struct _implicit_eq_params {
@@ -16,19 +17,32 @@ struct _implicit_eq_params {
 };
 
 
+/**
+ * Evaluate the value of implicit equation for the backward Euler method
+ *
+ * @param[in] a vector of displacement `dq` by time propagation by `dt` 
+ *     so that `q + dq` is the position at the next time point
+ * @param[in] extra parameters for evaluating the implicit equation
+ * @param[out] the vector-valued implicit equation
+ */
+
 int _implicit_eq(const gsl_vector *dqvec, void *params, gsl_vector *eq) {
 
 	struct _implicit_eq_params *const pp = (struct _implicit_eq_params *) params;	
 
+	const size_t Ndim = Bohm_Wavefunction_on_Box_1D::Ndim;
+
 	// Evaluate qvec at next time step, i.e. `qvec_next = qvec + dqvec`
 	// 
-	double qvec_next[Bohm_Wavefunction_on_Box_1D::Ndim];
-	for (size_t idim=0; idim<Bohm_Wavefunction_on_Box_1D::Ndim; ++idim)
-	{ qvec_next[idim] = pp->qvec[idim] + gsl_vector_get(dqvec, idim);	}
+	double qvec_next[Ndim];
+	qvec_next[0] = pp->qvec[0] + gsl_vector_get(dqvec, 0);
+//	for (size_t idim=0; idim<Ndim; ++idim)
+//	{ qvec_next[idim] = pp->qvec[idim] + gsl_vector_get(dqvec, idim);	}
 	
+
 	// Evaluate wf and grad_q_wf
 	// 
-	std::complex<double> wf_q, grad_q_wf[Bohm_Wavefunction_on_Box_1D::Ndim];
+	std::complex<double> wf_q, grad_q_wf[Ndim];
 	const bool check_wf_node = true;
 	int stat = Bohm_Wavefunction_on_Box_1D::wf_and_grad_q_wf(&wf_q, grad_q_wf, 
 			qvec_next, pp->wf_tot, pp->dx_grid, pp->xmin, pp->is0, check_wf_node);
@@ -71,9 +85,9 @@ int Bohm_Propagator_on_Box_1D::propagate(
 
 	// [NOTE] NULL and 0 should be replaced by appropriate one
 	struct _implicit_eq_params eq_params =
-	{ NULL, wf_tot, p_wf->get_dx(), xmin, dt, 1, 1, 0 };
+	{ NULL, wf_tot, p_wf->get_dx(), xmin, dt, hbar, mass, 0 };
 	
-	gsl_multiroot_function eq_f = {_implicit_eq, p_wf->Ndim, &eq_params};
+	gsl_multiroot_function eq_f = {&_implicit_eq, p_wf->Ndim, &eq_params};
 
 	std::complex<double> *const wf = wf_tot + 1;
 	if (EXIT_SUCCESS != prop_wf(wf, dt, prop_wf_params)) {
@@ -90,11 +104,22 @@ int Bohm_Propagator_on_Box_1D::propagate(
 }
 
 
+
+
+void print_state (size_t iq, double x, size_t iter, gsl_multiroot_fsolver *s) {
+  printf ("iq = %2lu xp = % .8f iter = %3lu dxp = % .8f "
+          "implicit_eq(dxp) = % .8e\n",
+          iq, x, iter,
+          gsl_vector_get (s->x, 0),
+          gsl_vector_get (s->f, 0));
+}
+
+
 int Bohm_Propagator_on_Box_1D::_propagate_core(
 		double *qarr, size_t Nq, gsl_multiroot_function *p_eq_f) 
 {
 	struct _implicit_eq_params *p_eq_params = 
-		(_implicit_eq_params *) p_eq_f->params;
+		(struct _implicit_eq_params *) p_eq_f->params;
 
 	const double xmin = p_eq_params->xmin;
 	const double xmax = p_wf->get_xmax(xmin);
@@ -113,19 +138,32 @@ int Bohm_Propagator_on_Box_1D::_propagate_core(
 			continue; 
 		}
 		// Prepare `eq_params`: set member `qvec`
-		p_eq_params->qvec = qarr + iq; // i.e. &qarr[iq];
+		p_eq_params->qvec = qarr + iq; // i.e. &qarr[iq];  // [suspicious]
 		// Prepare `eq_params`: set member `is0`
-		if (EXIT_SUCCESS !=	eval_is0(xp+dxp_init,Nx_tot,dx,xmin,&p_eq_params->is0))
+		if (EXIT_SUCCESS !=	eval_is0(xp+dxp_init,Nx_tot,dx,xmin,&p_eq_params->is0))  // [SUSPICOUS]
 		{
 			std::cerr << "[ERROR] Failed to evaluate `is0`\n";
 			return EXIT_FAILURE;
 		}
 		gsl_multiroot_fsolver_set(s, p_eq_f, dqvec); 
-		size_t i=0; const size_t max_iter = 200;
+		size_t i=0; size_t max_iter = 200;
 		int status;
+
+
+		gsl_vector *_fq_temp = gsl_vector_alloc(p_wf->Ndim);
+		(*(p_eq_f->f))(s->x, p_eq_params, _fq_temp);
+		std::cout << "fq_temp before iter: " << _fq_temp->data[0] << ", s->x: " << s->x->data[0] << std::endl;
+
+		print_state(iq, xp, i, s);
+
+		gsl_vector_free(_fq_temp);
+
 		for (; i<max_iter; ++i) {
+
 			status = gsl_multiroot_fsolver_iterate(s);
+			print_state(iq, xp, i+1, s);
 			if (status) { break; }
+
 			status = gsl_multiroot_test_residual(s->f, residual_tol);
 			if (status != GSL_CONTINUE) { break; }
 		}
@@ -136,6 +174,78 @@ int Bohm_Propagator_on_Box_1D::_propagate_core(
 		if (status != GSL_SUCCESS) {
 			std::cerr << "[ERROR] The iteration failed with error: " 
 				<< gsl_strerror(status) << std::endl;
+			std::cerr << "[ERROR] .. at xp = " << xp 
+				<< ", particle index (`iq`) = " << iq << ", is0 = " << p_eq_params->is0 
+				<< ", x_tot_arr[is0] = " << xmin + p_eq_params->dx_grid * p_eq_params->is0 << std::endl;
+
+			double x_is0 = xmin + p_eq_params->dx_grid * p_eq_params->is0;
+			double x_is0_max = x_is0 + p_eq_params->dx_grid * (FD_STENCIL_NUM-1);
+			size_t N_dxp_debug = 201;
+			double dx_dxp_debug = (x_is0_max - x_is0) / (N_dxp_debug - 1);
+			double *dxp_arr_debug = new double[N_dxp_debug];
+			for (size_t idxp=0; idxp<N_dxp_debug; ++idxp) { 
+				dxp_arr_debug[idxp] = (x_is0 + dx_dxp_debug * idxp) - xp;
+//				std::cout << dxp_arr_debug[idxp] << " ";
+			} std::cout << std::endl;
+			double *_impl_eq_dxp_arr_debug = new double[N_dxp_debug];
+
+			gsl_vector *_dxp_temp = gsl_vector_alloc(Bohm_Wavefunction_on_Box_1D::Ndim);
+			gsl_vector *_fq_temp = gsl_vector_alloc(Bohm_Wavefunction_on_Box_1D::Ndim);
+
+			for (size_t idxp=0; idxp<N_dxp_debug; ++idxp) {
+				gsl_vector_set(_dxp_temp, 0, dxp_arr_debug[idxp]);
+				(*(p_eq_f->f))(_dxp_temp, p_eq_params, _fq_temp);
+				_impl_eq_dxp_arr_debug[idxp] = gsl_vector_get(_fq_temp, 0);
+//				std::cerr << _impl_eq_dxp_arr_debug[idxp] << " ";
+			} std::cerr << "\n";
+
+
+			std::ofstream dxp_arr_file("debug-dxp-arr.bin");
+			dxp_arr_file.write((char *) dxp_arr_debug, N_dxp_debug * sizeof(double));
+			dxp_arr_file.close();
+			std::ofstream impl_eq_dxp_arr_file("debug-impl-eq-dxp-arr.bin");
+			impl_eq_dxp_arr_file.write((char *) _impl_eq_dxp_arr_debug, N_dxp_debug * sizeof(double));
+			impl_eq_dxp_arr_file.close();
+
+
+			std::complex<double> _wf_q_temp, _grad_q_wf_temp[Bohm_Wavefunction_on_Box_1D::Ndim];
+			std::complex<double> *_wf_q_temp_arr = new std::complex<double>[N_dxp_debug];
+
+			double *_debug_velo_arr = new double[N_dxp_debug];
+
+			double _qvec_temp[Bohm_Wavefunction_on_Box_1D::Ndim];
+			for (size_t idxp=0; idxp<N_dxp_debug; ++idxp) {
+				_qvec_temp[0] = xp + dxp_arr_debug[idxp];
+				p_wf->wf_and_grad_q_wf(&_wf_q_temp, _grad_q_wf_temp, _qvec_temp, 
+						p_eq_params->wf_tot, p_eq_params->dx_grid, p_eq_params->xmin, p_eq_params->is0, true);
+				_wf_q_temp_arr[idxp] = _wf_q_temp;
+				_debug_velo_arr[idxp] = (p_eq_params->hbar / p_eq_params->mass) * std::imag(_grad_q_wf_temp[0] / _wf_q_temp);
+			}
+
+			std::ofstream wf_q_temp_arr_file("debug-wf-dxp-arr.bin");
+			wf_q_temp_arr_file.write((char *) _wf_q_temp_arr, N_dxp_debug * sizeof(std::complex<double>));
+			wf_q_temp_arr_file.close();
+			delete [] _wf_q_temp_arr;
+
+			std::ofstream velo_temp_arr_file("debug-velocity-dxp-arr.bin");
+			velo_temp_arr_file.write((char *) _debug_velo_arr, N_dxp_debug * sizeof(double));
+			velo_temp_arr_file.close();
+			delete [] _debug_velo_arr;
+
+
+
+			(*(p_eq_f->f))(dqvec, p_eq_params, _fq_temp);
+//			(*(p_eq_f->f))(xp+0., p_eq_params, 
+			std::cout << "implicit_eq(0,...): " << _fq_temp->data[0] << ", dqvec[0]: " << dqvec->data[0] << std::endl;
+			(*(p_eq_f->f))(s->x, p_eq_params, _fq_temp);
+			std::cout << "implicit_eq(s->x,...): " << _fq_temp->data[0] << ", s->x[0]: " << s->x->data[0] << std::endl;
+			std::cout << "s->f: " << s->f->data[0] << std::endl;
+
+			gsl_vector_free(_fq_temp);
+			gsl_vector_free(_dxp_temp);
+			delete [] dxp_arr_debug;
+			delete [] _impl_eq_dxp_arr_debug;
+
 			return EXIT_FAILURE;
 		}
 		
